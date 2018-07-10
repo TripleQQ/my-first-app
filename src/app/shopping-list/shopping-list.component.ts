21be683c40fddcd2055c5f086ccf5e56acc3a5c7
zhangqiang.liu <zhangqiang.liu@wizvision.com>
2018-07-11 00:28:39 +0800

    basic layout for shopping

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,12 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-
+import {Ingredient} from '../share/ingredient.model';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+ingredients:Ingredient[]=[
+new Ingredient('apple',5),
+new Ingredient('orange',10)
+];
   constructor() { }
 
   ngOnInit() {
